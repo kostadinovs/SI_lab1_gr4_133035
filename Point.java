e3bd54e12d648aa93f66cb6d3ee39910b5be56b1
Stole Kostadinv <stolek@outlook.com>
2020-04-12 12:38:35 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -7,7 +7,46 @@ class Point {
 
 	//TODO constructor
 
+	public Point(String id, double x, double y, String colorPoints) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.colorPoints = colorPoints;
+	}
 	//TODO setters and getters
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getColorPoints() {
+		return colorPoints;
+	}
+
+	public void setColorPoints(String colorPoints) {
+		this.colorPoints = colorPoints;
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
